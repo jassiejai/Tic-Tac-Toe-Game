8594bd88fec0cb8a8f25fa130ba060f47fab9dd8
Jassie <jasminehart94@gmail.com>
2021-12-27 14:04:02 -0800

    made some changes

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// clicking the button starts a new game
-// addan event listener for click to run fucntion
+//created an array to hold the vaule of X or O
+let ticTac = ["","","","","","","","",""]
 
+
+
+
+// clicking the button starts a new game
+// add an event listener for click to run fucntion
 document.getElementById("start").addEventListener("click" , startGame)
 
 function startGame(){
-    let stPlayer = prompt ('Choose your player choose 1 or 2')
+    let stPlayer = prompt ('Choose X or O')
 
-    if (stPlayer == 1) {
+    if (stPlayer == 'O') {
         playOne()
-    } else if (stPlayer == 2) {
+    } else if (stPlayer == 'X') {
         playTwo()
     }
     else {
@@ -24,38 +29,64 @@ document.getElementsByClassName('one')
 // choose an x or o
 
 function xAndO() {
+//this loops the array of ticTac with the turns.
+  for(let t = 0; t< ticTac.length; t++){
+      turns([t])
+  }  
 //click the div it enters a value of x or o
 // create button that submite you box selection
 }
+let odd = [true,false,true,false,true,false,true,false,true]
 
 // create a fucntion that toogles between two characters depending on the first choosen character
-function turns() {
-   // if xAndO is x then the next turn you should expect O it shouldnt allow for the x to be choosen 
-}
-//create a function for each player
+odd.forEach(function turns() {
+    //pulls every odd index item and loops it
+ for (let turn of odd){
+   if (turn == true){
+       return "playOne"
+   }
+   else if (turn == false){
+       return "playTwo"
+   }
+   else {
+       break
+   }
+  }  // if xAndO is x then the next turn you should expect O it shouldnt allow for the x to be choosen 
+})
 
+
+//create a function for each player
+//click and item and it fills with x or o
 function playOne() {
     alert ("You have choosen player one")
+    document.getElementsByClassName('board').addEventListener("click", (tac) => {
+        
+    } )
+    const tac = "O"
+    
   // the function should let you choose your piece and tell you who goes first
 }
 
 function playTwo() {
     alert ("You have choosen player two")
-
+    const tic = "X"
     
 }
 
 // click the div and apply X or O using an event listener to it would i have to first work of attaching the value to a player or 
 
 
+//choose the vaule of wither x or o and add the value to the player
+
 
 
 
 // create a function that notifies if the game is won lost or tied if not return an alert
+//if not equal to 3 show this if = to 3 show this else player ones turn play twos turn
 //end game functionalluty until the new game button is hit but doesnt refresh the page 
 
 
 
 
 // create a grid div with 9 other divs to the create a grid with. can used the numbered div to be able to run possible outcomes of three in a row
-// add the grid styling then finish the styling (create a border and apply it to two sides)
+// add the grid styling in the parent div using 1fr with spaces then finish the styling (create a border and apply it to two sides)
